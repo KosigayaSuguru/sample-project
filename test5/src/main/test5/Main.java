63a3a05d35b4717f691346c22ec4326f6b44f4e8
KosigayaSuguru <asd7220017@gmail.com>
2016-01-27 00:58:11 +0900

    Streamの練習

--- test5/src/main/test5/Main.java
+++ test5/src/main/test5/Main.java
@@ -1,6 +1,11 @@
 package main.test5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import factory.test5.ServiceFactory;
 import inter.test5.Service;
@@ -13,6 +18,24 @@ public class Main {
 		ArrayList<String> service = new ArrayList<String>();
 		service.add("TEST1");
 		service.add("TEST2");
+
+		class Test {
+			public int uho;
+
+			public Test(int a){
+				this.uho = a;
+			}
+			public int getUho() {
+				return uho;
+			}
+		}
+
+		//stream test->
+		Stream<String> aaa = Stream.of("hoge","hage");
+		System.out.println("aaa " + aaa.anyMatch(e -> e.equals("hoge")));
+		List<Test> list = Arrays.asList(new Test(11),new Test(12),new Test(13));
+		Map<String, Integer> bbb = list.stream().map(e->"aa" + e.getUho() + "aa").collect(Collectors.toMap(s->s,s->s.length()));
+		//stream test<-
 
 		for(String str : service){
 			Service hoge = ServiceFactory.create(str);
