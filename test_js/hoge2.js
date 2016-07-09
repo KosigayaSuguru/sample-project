46aa7518182c6cc7ab178f70defe62551943dace
KosigayaSuguru <asd7220017@gmail.com>
2016-07-10 01:18:05 +0900

    js

--- test_js/hoge2.js
+++ test_js/hoge2.js
@@ -23,6 +23,7 @@ function hoge(){
 	url:"https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://kancolle.doorblog.jp/index.rdf&num=-1",
 	success: function(data){
 		res = data;
+		$.each(res.responseData.feed.entries, function(){ $('#list1').append(this.title + "<br>")})
 		},
 	error: function(data, a, b){
 		$("#error_msg").text("error").css("color","red");
