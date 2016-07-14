2dad7418e73c3455f315889dc75383dcb12bca88
KosigayaSuguru <asd7220017@gmail.com>
2016-07-15 00:58:13 +0900

    fix

--- test_js/hoge2.js
+++ test_js/hoge2.js
@@ -1,34 +1,46 @@
-var res;
-
 $(function(){
-//alert("test");
+	//alert("test");
 
-$("#test").on("click",hoge);
+	var res;
+	var entryId = 0;
 
-Hoge = function(){};
+	$("#test").on("click",createList);
 
-var hoge = new Hoge();
-Hoge.prototype.p1 = "test1";
-Hoge.prototype.p2 = "test2";
-Hoge.prototype.p3 = "test3";
+	Hoge = function(){};
 
+	var hoge = new Hoge();
+	Hoge.prototype.p1 = "test1";
+	Hoge.prototype.p2 = "test2";
+	Hoge.prototype.p3 = "test3";
 
-function hoge(){
-	var moge = new Hoge();
-	//alert(moge.p1);
-	//alert(hoge.p2);
-	$.ajax({
-	type: "GET",
-	dataType: "jsonp",
-	url:"https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://kancolle.doorblog.jp/index.rdf&num=-1",
-	success: function(data){
-		res = data;
-		$.each(res.responseData.feed.entries, function(){ $('#list1').append(this.title + "<br>")})
-		},
-	error: function(data, a, b){
-		$("#error_msg").text("error").css("color","red");
-		alert("error");
-		}
-	});
-};
+	function createList(){
+		var moge = new Hoge();
+		//alert(moge.p1);
+		//alert(hoge.p2);
+		$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		async: false,
+		url:"https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://kancolle.doorblog.jp/index.rdf&num=-1",
+		success: function(data){
+			res = data;
+
+			$.each(res.responseData.feed.entries, function(){
+				var apend = addList(this);
+				$('#list1').append(apend)})
+			},
+		error: function(data, a, b){
+			$("#error_msg").text("error").css("color","red");
+			alert("error");
+			}
+		});
+	};
+
+	 function addList(arg){
+	 	var arg1 = arg;
+		var tmp = "<span style=\"display:block;margin-bottom:4px;\" id=entryId" + entryId + "><a href=\""+ arg1.link + "\"target=_blank>" + arg1.title + "</a></span>";
+		entryId++;
+		return tmp;
+	}
 });
+
