b93e7c7ab9d36b0a8980afa56e68399d64b5a54f
KosigayaSuguru <asd7220017@gmail.com>
2016-08-31 23:23:38 +0900

    AOPで出力するログを修正

--- test3/src/main/java/test3/TestAspect.java
+++ test3/src/main/java/test3/TestAspect.java
@@ -3,6 +3,8 @@ package test3;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,10 +12,18 @@ import org.springframework.stereotype.Component;
 public class TestAspect {
     @Around("execution(public * test3..*.*(..))")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        System.out.println(this.getClass().getName() + "before");
-        Object obj = point.proceed();
-        System.out.println(this.getClass().getName() + "after");
 
-        return obj;
+    	Logger logger = LoggerFactory.getLogger(point.getTarget().getClass());
+    	try {
+            logger.info("BEGIN " + point.getSignature().toString());
+            Object obj = point.proceed();
+            logger.info("END " + point.getSignature().toString());
+
+            return obj;
+
+    	} catch (Exception e) {
+    		logger.warn("EXCEPTION " + point.getSignature().toString());
+	        throw e;
+		}
     }
 }
