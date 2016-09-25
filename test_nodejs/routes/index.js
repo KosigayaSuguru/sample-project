a1cd071b289c24c995754078ab7aa9b65a322a30
self <self>
2016-09-26 00:33:42 +0900

    [nodejs] axiosとasyncを合わせる

--- test_nodejs/routes/index.js
+++ test_nodejs/routes/index.js
@@ -6,11 +6,29 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    axios.get('http://www.yahoo.co.jp').then(function(res) {
-        console.log("axios response:" + res.status);
+
+    // 他サーバにget
+    async.series([function(callback) {
+        axios.get('http://www.yahoo.co.jp', { timeout: 1000 }).then(function(res) {
+            console.log("axios response:" + res.status);
+            callback(null, 'success axios');
+
+            // 他サーバへのリクエストに失敗した時
+        }).catch(function(err) {
+            callback(err, 'error axios');
+        });
+
+        //他サーバへのgetに成功した時の次の処理
+    }, function(callback) {
+        console.log("render get /");
+        res.render('index', { title: 'Express' });
+
+        // seriesの中でエラーがあった時
+    }], function(err, results) {
+        console.error(results);
+        console.error(err);
+        next(err);
     });
-    console.log("render get /");
-    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
