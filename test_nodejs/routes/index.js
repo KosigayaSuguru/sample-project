920935e27b941c6fcb7699f251e3b5fd8babbc65
self <self>
2016-09-26 00:17:32 +0900

    [nodejs] axios対応

--- test_nodejs/routes/index.js
+++ test_nodejs/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var axios = require('axios');
+var async = require('async');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    axios.get('http://www.yahoo.co.jp').then(function(res) {
+        console.log("axios response:" + res.status);
+    });
+    console.log("render get /");
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
