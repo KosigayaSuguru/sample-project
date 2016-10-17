9d35714a94995bec9b0b11f1e977245683c269e2
self <self>
2016-10-17 22:42:40 +0900

    JMS対応（@JmsListener対応）

--- test3/src/main/java/test3/web/controller/TestController7.java
+++ test3/src/main/java/test3/web/controller/TestController7.java
@@ -3,6 +3,7 @@ package test3.web.controller;
 import javax.jms.JMSException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.messaging.Message;
@@ -42,31 +43,29 @@ public class TestController7 extends WebApplicationObjectSupport {
 
 	// HornetQの起動→の普通に起動するだけ:"D:\java\hornetq-2.4.0.Final\bin\run.bat"
 	// D:\java\hornetq-2.4.0.Final\config\stand-alone\non-clustered\hornetq-jms.xml 内の queue で指定されているキューを使う必要がある
-	// 先に /JmsTest2 へアクセスし、receiveを起動してから、/JmsTestをへアクセスすると、TestBeanの値が、/JmsTest2へ渡される
+	// 先に /JmsTest へExpiryQueueへ メッセージを投げて、DLQを待ち受けする。
+	// @JmsListener(destination = "ExpiryQueue")がメッセージを受け取って処理を始める。
+	// 処理が終わると DLQ へメッセージを投げて、/JmsTestの方が処理始める。
 	@RequestMapping("/JmsTest")
 	public String jmsTest() throws JMSException {
 
 		TestBean test = new TestBean();
-		test.setHoge("jms hoge");
-		test.setMoge("jms moge");
+		test.setHoge("jms hoge" + a++);
+		test.setMoge("jms moge" + a++);
 
 		jmsMessagingTemplate.convertAndSend("ExpiryQueue", test);
+
+		Message<?> message = jmsMessagingTemplate.receive("DLQ");
+		System.err.println("aaaaaaaaaaaaaaaaaaaaaa" + message.toString());
 
 		return test.toString();
 	}
 
-//	@JmsListener(destination = "testQueue")
-//	public void receive(TestBean todo) {
-//		// omitted
-//		System.err.println("JMS TEST");
-//	}
-
-	@RequestMapping("/JmsTest2")
-	public void receive(TestBean todo) {
-
-		Message<?> message = jmsMessagingTemplate.receive("ExpiryQueue");
-
+	@JmsListener(destination = "ExpiryQueue")
+	public void receive(TestBean message) {
 		// omitted
-		System.err.println(message.toString());
+		System.err.println("bbbbbbbbbbbbbbbbbbb" + message.toString());
+
+		jmsMessagingTemplate.convertAndSend("DLQ", message);
 	}
 }
