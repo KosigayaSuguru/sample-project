f4e0aecf7ace68e9ee164afca9ab8759a4bc5af2
self <self>
2016-12-27 22:19:42 +0900

    dbdump

---
+++ dbdump_schema.sql
@@ -0,0 +1,28 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test1` (
+  `id` int(11) NOT NULL,
+  `test1_col1` int(11) DEFAULT NULL,
+  `test1_col2` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test2` (
+  `id` int(11) NOT NULL,
+  `test1_id` int(11) DEFAULT NULL,
+  `test2_col1` int(11) DEFAULT NULL,
+  `test2_col2` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test3` (
+  `id` int(11) NOT NULL,
+  `enum_test` enum('ENUM_VAL1','ENUM_VAL2','ENUM_VAL3') DEFAULT NULL,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
