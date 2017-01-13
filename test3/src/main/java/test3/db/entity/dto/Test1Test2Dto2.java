927391c8faf91d5c11f8c8d3a3d4b0bf34373439
self <self>
2017-01-14 00:58:37 +0900

    MyBatisサンプルを修正2

--- test3/src/main/java/test3/db/entity/dto/Test1Test2Dto2.java
+++ test3/src/main/java/test3/db/entity/dto/Test1Test2Dto2.java
@@ -1,33 +1,31 @@
 package test3.db.entity.dto;
 
-import java.util.List;
-
 import test3.db.entity.Test1;
 import test3.db.entity.Test2;
 
 public class Test1Test2Dto2 {
 
 	private Test1 test1;
-	private List<Test2> test2List;
+	private Test2 test2;
 
 	public Test1 getTest1() {
 		return test1;
 	}
 
-	public List<Test2> getTest2List() {
-		return test2List;
+	public Test2 getTest2() {
+		return test2;
 	}
 
 	public void setTest1(Test1 test1) {
 		this.test1 = test1;
 	}
 
-	public void setTest2List(List<Test2> test2List) {
-		this.test2List = test2List;
+	public void setTest2(Test2 test2) {
+		this.test2 = test2;
 	}
 
 	@Override
 	public String toString() {
-		return "Test1Test2Dto [test1=" + test1 + ", test2List=" + test2List + "]";
+		return "Test1Test2Dto2 [test1=" + test1 + ", test2=" + test2 + "]";
 	}
 }
