6320515e9b60808bc80cc85f31f17547fab3f63f
self <self>
2017-01-15 17:57:11 +0900

    MｙBatisサンプル修正

--- test3/src/main/java/test3/db/entity/dto/Test1Test2Dto2.java
+++ test3/src/main/java/test3/db/entity/dto/Test1Test2Dto2.java
@@ -2,11 +2,13 @@ package test3.db.entity.dto;
 
 import test3.db.entity.Test1;
 import test3.db.entity.Test2;
+import test3.db.entity.Test3;
 
 public class Test1Test2Dto2 {
 
 	private Test1 test1;
 	private Test2 test2;
+	private Test3 test3;
 
 	public Test1 getTest1() {
 		return test1;
@@ -14,6 +16,10 @@ public class Test1Test2Dto2 {
 
 	public Test2 getTest2() {
 		return test2;
+	}
+
+	public Test3 getTest3() {
+		return test3;
 	}
 
 	public void setTest1(Test1 test1) {
@@ -24,8 +30,13 @@ public class Test1Test2Dto2 {
 		this.test2 = test2;
 	}
 
+	public void setTest3(Test3 test3) {
+		this.test3 = test3;
+	}
+
 	@Override
 	public String toString() {
 		return "Test1Test2Dto2 [test1=" + test1 + ", test2=" + test2 + "]";
 	}
+
 }
