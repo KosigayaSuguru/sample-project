43647c5c8800e9fce78eaf878b811fd0ade22578
self <self>
2017-05-28 00:58:12 +0900

    非同期サンプルのHttpクライアントをOkHttpに変更

--- test3/src/main/java/test3/web/controller/AsyncController.java
+++ test3/src/main/java/test3/web/controller/AsyncController.java
@@ -2,20 +2,24 @@ package test3.web.controller;
 
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.support.WebApplicationObjectSupport;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.options.Option;
-import com.mashape.unirest.http.options.Options;
-import com.mashape.unirest.http.utils.SyncIdleConnectionMonitorThread;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 @RestController
 public class AsyncController extends WebApplicationObjectSupport {
@@ -23,37 +27,59 @@ public class AsyncController extends WebApplicationObjectSupport {
 	@RequestMapping(value = "/Async1", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String async1() throws Exception {
 
-		String yql = "";
-		String url = "";
-		String params = "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback=";
+		ExecutorService requestExecutor = Executors.newFixedThreadPool(40);
 
-		ExecutorService a = Executors.newFixedThreadPool(10);
-		SyncIdleConnectionMonitorThread monitor = (SyncIdleConnectionMonitorThread) Options.getOption(Option.SYNC_MONITOR);
-		monitor.interrupt();
+		ArrayList<FutureTask<String>> list = new ArrayList<>();
 
-		Future<String> submit = a.submit(() -> {
-			return "hoge";
-		});
+		Logger logger = LoggerFactory.getLogger("Async1");
 
-		ArrayList<Future<HttpResponse<String>>> list = new ArrayList<>();
+		// idxページ分のリクエストを非同期に投げる
+		// ※ Executor分だけ同時にリクエストし、OkHttpのプールが生成される
+		OkHttpClient okHttpClient = new OkHttpClient();
 		// YQL: select * from html where url='http://kancolle.doorblog.jp/?p=1' and xpath='//h2[@class="article-title entry-title"]//a'
-		for (int idx = 1; idx < 100; idx++) {
-			yql = String.format(
-					"select * from html where url='http://kancolle.doorblog.jp/?p=%d' and xpath='//h2[@class=\"article-title entry-title\"]//a'",
-					idx);
-			url = "https://query.yahooapis.com/v1/public/yql?q=" + URLEncoder.encode(yql, "UTF-8") + params;
+		for (int idx = 1; idx < 200; idx++) {
 
-			Future<HttpResponse<String>> asStringAsync = Unirest.get(url).asStringAsync();
-			list.add(asStringAsync);
+			final int idx_ = idx;
+			FutureTask<String> aa = new FutureTask<>(() -> {
+				String yql = String.format(
+						"select * from html where url='http://kancolle.doorblog.jp/?p=%d' and xpath='//h2[@class=\"article-title entry-title\"]//a'",
+						idx_);
+				String params = "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback=";
+				String url = "https://query.yahooapis.com/v1/public/yql?q=" + URLEncoder.encode(yql, "UTF-8") + params;
+
+				Request req = new Request.Builder().url(url).build();
+				okhttp3.Response res = okHttpClient.newCall(req).execute();
+
+				logger.debug(String.format("pool %d, url:%s", okHttpClient.connectionPool().connectionCount(), url));
+
+				return res.body().string();
+			});
+			requestExecutor.submit(aa);
+			list.add(aa);
 		}
 
-		String string = submit.get();
+		ObjectMapper jackson = new ObjectMapper();
+		LinkedHashMap<String, Object> ret = new LinkedHashMap<>();
+		ArrayList<Object> retval = new ArrayList<>();
 
-		for (Future<HttpResponse<String>> future : list) {
-			HttpResponse<String> aa = future.get();
-			System.err.println(aa.getBody());
+		// リクエスト数分の結果を取得してjson返却用のマップを生成する
+		for (FutureTask<String> futureTask : list) {
+			String tmp = futureTask.get();
+
+			LinkedHashMap<String, Object> tmpmap = new LinkedHashMap<>();
+
+			tmpmap = jackson.readValue(tmp, LinkedHashMap.class);
+			@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
+			List<LinkedHashMap<String, String>> result = (List) ((Map) ((Map) tmpmap.get("query")).get("results")).get("a");
+
+			result.forEach((item) -> {
+				item.remove("title");
+				item.remove("rel");
+				retval.add(item);
+			});
 		}
 
-		return string;
+		ret.put("urls", retval);
+		return jackson.writeValueAsString(ret);
 	}
 }
