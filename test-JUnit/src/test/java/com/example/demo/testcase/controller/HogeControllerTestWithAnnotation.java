b3e6852c1113dd1daad4663ed985fcc567198b99
self2 <self2>
2018-05-13 11:51:22 +0900

    test-JUnit: Controllerの中で使用されるBean（Aとする）をSpy化して、Aの中のBeanを更にモック化した状態のテストを追加

--- test-JUnit/src/test/java/com/example/demo/testcase/controller/HogeControllerTestWithAnnotation.java
+++ test-JUnit/src/test/java/com/example/demo/testcase/controller/HogeControllerTestWithAnnotation.java
@@ -16,21 +16,31 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import com.example.demo.controller.HogeController;
 import com.example.demo.controller.HogeController.HogeJson;
 import com.example.demo.models.TestBean;
+import com.example.demo.models.TestBeanChild;
 
 /**
- * HogeControllerに対して、なるべくアノテーションを使用してテストを実行する。
- * @Mock、@InjectMockを使ってテストを実行している。
+ * HogeController に対して、なるべくアノテーションを使用してテストを実行する。
+ * @Mock、@InjectMock を使ってテストを実行している。
+ * @Spy は使おうとするとエラーがでるので、PowerMockito.spy() を使っている。
+ * 
+ * 構成としては、
+ * ・TestBean の中で使用している TestBeanChild のモックとして、testBeanChildMock を使う (testBeanSpy に@InjectMocksを付与している)
+ * ・HogeController の中で使用している TestBean のモックとして、testBeanSpy を使う (hogeControllerSpy に@InjectMocksを付与している)
+ * 
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ HogeController.class })
 public class HogeControllerTestWithAnnotation {
 
 	@InjectMocks
-	private HogeController c = PowerMockito.spy(new HogeController());
+	private HogeController hogeControllerSpy = PowerMockito.spy(new HogeController());
+
+	@InjectMocks
+	private TestBean testBeanSpy = PowerMockito.spy(new TestBean());
 
 	@Mock
-	private TestBean b;
-
+	private TestBeanChild testBeanChildMock;
+	
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
@@ -53,14 +63,16 @@ public class HogeControllerTestWithAnnotation {
 		// @Spyを使おうとするとエラーが出るので、Spy()だけPowerMockitoのメソッドを直接呼び出している
 
 		// モックの設定
-		PowerMockito.when(b.getA()).thenReturn("mock TestBean.getA()");
-		PowerMockito.when(c, "testPublicMethod").thenReturn("public method mock return");
-		PowerMockito.when(c, "testPrivateMethod").thenReturn("private method mock return");
+		PowerMockito.when(testBeanSpy.getA()).thenReturn("mock TestBean.getA()");
+		PowerMockito.when(testBeanChildMock.getA()).thenReturn("mock TestBeanChild.getA()");
+		PowerMockito.when(hogeControllerSpy, "testPublicMethod").thenReturn("public method mock return");
+		PowerMockito.when(hogeControllerSpy, "testPrivateMethod").thenReturn("private method mock return");
 
-		HogeJson hoge = c.hoge();
+		HogeJson hoge = hogeControllerSpy.hoge();
 		assertEquals("public method mock return", hoge.pub);
 		assertEquals("private method mock return", hoge.pri);
 		assertEquals("mock TestBean.getA()", hoge.testBeanA);
+		assertEquals("mock TestBeanChild.getA()", hoge.testBeanChildA);
 	}
 
 	@Test
@@ -71,6 +83,6 @@ public class HogeControllerTestWithAnnotation {
 		// その設定が生きてないっていう（メソッドセーフみたいな感じ）ことの確認用
 		// ※何度か実行して、コンソールに 1 2 null ってでればOK
 
-		System.out.println(b.getA()); // nullになる
+		System.out.println(testBeanSpy.getA()); // nullになる
 	}
 }
